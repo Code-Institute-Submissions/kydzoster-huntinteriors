51fce586682a543b25e53f93d3c5089abeb8693f
Martins Silins <kydzoster@gmail.com>
2020-09-07 14:50:25 +0000

    added individual gallery details

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gallery, name='gallery')
+    path('', views.gallery, name='gallery'),
+    path('<gallery_id>', views.gallery_detail, name='gallery_detail'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Gallery
 
 
-# Will show all products, sorting and search
+# Will show all gallery, sorting and search
 def gallery(request):
 
     gallery = Gallery.objects.all()
@@ -11,3 +11,14 @@ def gallery(request):
         'gallery': gallery
     }
     return render(request, 'gallery/gallery.html', context)
+
+
+# Will show individual image details
+def gallery_detail(request, gallery_id):
+
+    gallery_detail = get_object_or_404(Gallery, pk=gallery_id)
+
+    context = {
+        'gallery_detail': gallery_detail
+    }
+    return render(request, 'gallery/gallery_detail.html', context)
