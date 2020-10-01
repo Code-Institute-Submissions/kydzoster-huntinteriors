a33884558e12142b4581baf5c02d1ea81013be17
Martins Silins <kydzoster@gmail.com>
2020-10-01 14:08:40 +0000

    updated settings

--- hunt_interiors/settings.py
+++ hunt_interiors/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['kydzoster-hunt-interiors.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['hunt-int.herokuapp.com', 'localhost']
 
 
 # Application definition
