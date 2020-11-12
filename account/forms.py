c4f9251f7e28321d1a1eb9f50175a212801f88af
Martins Silins <kydzoster@gmail.com>
2020-11-12 11:23:33 +0000

    fixed Gallery and Delivery navbar

--- account/forms.py
+++ account/forms.py
@@ -29,10 +29,3 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
-
-
-# will allow to edit date of birth and profile picture
-class ProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('photo', )
--- account/models.py
+++ account/models.py
@@ -6,7 +6,6 @@ class Profile(models.Model):
     # when user is deleted, its profile is also deleted
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='users/%d/%m/%Y/', blank=True)
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
