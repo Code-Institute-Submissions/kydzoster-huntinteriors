f0a6ceed2aa7b8595d6f5a8dd32254f47db3b12a
Martins Silins <kydzoster@gmail.com>
2020-12-15 15:56:24 +0000

    db

--- hunt_interiors/settings.py
+++ hunt_interiors/settings.py
@@ -131,6 +131,13 @@ WSGI_APPLICATION = 'hunt_interiors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
